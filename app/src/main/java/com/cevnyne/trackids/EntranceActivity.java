28e837b9736c5d2ea98ef2c98a3bb522e6f0d5c2
NN Fathoni <31845226+nfathoni79@users.noreply.github.com>
2020-01-27 11:18:06 +0700

    Updated permission for higher API level

--- app/src/main/java/com/cevnyne/trackids/EntranceActivity.java
+++ app/src/main/java/com/cevnyne/trackids/EntranceActivity.java
@@ -1,8 +1,13 @@
 package com.cevnyne.trackids;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.test.mock.MockPackageManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -24,10 +29,23 @@ public class EntranceActivity extends AppCompatActivity {
     private Button mSignInButton;
     private Button mSignUpButton;
 
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+    String[] mPermissions = {mPermission};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrance);
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, mPermission) != MockPackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, mPermissions, REQUEST_CODE_PERMISSION);
+                // If any permission above not allowed by user, this condition will execute every time, else your else part will work
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
@@ -76,6 +94,21 @@ public class EntranceActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT).show();
                 }
             });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.e("Req code", "" + requestCode);
+
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (grantResults.length == 1 && grantResults[0] == MockPackageManager.PERMISSION_GRANTED) {
+                // Success stuff here
+            } else {
+                // Failure here
+            }
         }
     }
 
